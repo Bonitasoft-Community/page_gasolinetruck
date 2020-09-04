1aad1ec80b5d9bec86e9c105fa0967dca6bf8460
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-09-03 17:45:29 -0700

    1.7.0 Ready for 7.11 + Postgres

--- Actions.groovy
+++ Actions.groovy
@@ -32,9 +32,10 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
 import org.apache.commons.lang3.StringEscapeUtils
 
-import org.bonitasoft.console.common.server.page.PageContext
-import org.bonitasoft.console.common.server.page.PageController
-import org.bonitasoft.console.common.server.page.PageResourceProvider
+import org.bonitasoft.web.extension.page.PageContext;
+import org.bonitasoft.web.extension.page.PageController;
+import org.bonitasoft.web.extension.page.PageResourceProvider;
+
 import org.bonitasoft.engine.exception.UnknownAPITypeException;
 
 import org.bonitasoft.engine.api.TenantAPIAccessor;
